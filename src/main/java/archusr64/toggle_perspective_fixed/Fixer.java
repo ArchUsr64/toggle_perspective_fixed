cc343484306790b31081f907e548aecd768560ed
ArchUsr64 <anshulusr@gmail.com>
2023-05-27 20:20:58 +0530

    fixed mouse event not toggling perspective

--- src/main/java/archusr64/toggle_perspective_fixed/Fixer.java
+++ src/main/java/archusr64/toggle_perspective_fixed/Fixer.java
@@ -3,8 +3,14 @@ package archusr64.toggle_perspective_fixed;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.MouseInputEvent;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 
 @Mod(modid = "toggle_perspective_fixed", version = "1.0.0", clientSideOnly = true)
 public class Fixer {
@@ -12,5 +18,27 @@ public class Fixer {
     public void init(FMLInitializationEvent event) {
         // some example code
         System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onInput(MouseInputEvent event) {
+        Minecraft game = Minecraft.getMinecraft();
+        GameSettings settings = game.gameSettings;
+        if (settings.keyBindTogglePerspective.isPressed()) {
+            ++settings.thirdPersonView;
+
+            if (settings.thirdPersonView > 2) {
+                settings.thirdPersonView = 0;
+            }
+
+            if (settings.thirdPersonView == 0) {
+                game.entityRenderer.loadEntityShader(game.getRenderViewEntity());
+            } else if (settings.thirdPersonView == 1) {
+                game.entityRenderer.loadEntityShader((Entity) null);
+            }
+
+            game.renderGlobal.setDisplayListEntitiesDirty();
+        }
     }
 }
