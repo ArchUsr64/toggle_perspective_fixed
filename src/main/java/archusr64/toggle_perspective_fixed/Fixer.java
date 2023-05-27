56929ead04003dcd0bdf8c74330771c591fd2be2
ArchUsr64 <anshulusr@gmail.com>
2023-05-27 19:12:28 +0530

    initialise mod config

--- src/main/java/archusr64/toggle_perspective_fixed/Fixer.java
+++ src/main/java/archusr64/toggle_perspective_fixed/Fixer.java
@@ -1,17 +1,16 @@
-package studio.dreamys;
+package archusr64.toggle_perspective_fixed;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.event.MouseEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+@Mod(modid = "toggle_perspective_fixed", version = "1.0.0", clientSideOnly = true)
+public class Fixer {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-		// some example code
+        // some example code
         System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
     }
 }
